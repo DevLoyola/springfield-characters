8f9e244c07cefdb144d8f963f6de53f6c35e99d6
Danielle Loyola <danielle.a.loyola@gmail.com>
2023-09-16 17:11:46 +1200

    installed axios, added button to get data from API

--- springfield-characters/src/App.js
+++ springfield-characters/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const getCharacter = () => {
+    axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+    .then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
       <h1>Springfield</h1>
-      </header>
+      <button onClick = {getCharacter}>Get Springfield Charater</button>
     </div>
   );
 }
